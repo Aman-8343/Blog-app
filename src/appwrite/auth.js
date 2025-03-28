4235102b917bdc55159acb562c661d9c8690287a
Aman kumar <amankumar105541@gmail.com>
2025-03-28 14:29:07 +0530

    Build Authentication service

---
+++ src/appwrite/auth.js
@@ -0,0 +1,55 @@
+import config from "../config/config";
+
+import { Client, Account,ID} from 'appwrite';
+
+export class AuthService{
+    client= new Client();
+    Account;
+
+    constructor(){
+        this.client
+        setEndPoint(config.appwriteurl)
+        setProject(config.appwriteProjectId);
+        this.Account=new Account(this.Account)
+    }
+    async createAccount({email,password,name}){
+        try {
+       const userAccount=   await this.Account.create(ID.unique(),email,password,name)  ;
+       if (userAccount) {
+         return this.login({email,password});
+       } else {
+        return userAccount;
+       }
+        } catch (error) {
+            throw error;
+        }
+    }
+    async login({email,password}){
+        try {
+           return await this.Account.createEmailSession(email,password);
+        } catch (error) {
+           throw error 
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+             return await this.Account.get();
+        } catch (error) {
+            throw error
+        }
+        return null;
+    }
+
+    async Logot(){
+        try {
+            await this.Account.deleteSessions();
+        } catch (error) {
+            console.log("appwritebservice :: Logout :: error",error);
+        }
+    }
+}
+
+const AuthService=new AuthService();
+
+export default AuthService

af9c0935a51e546543c3f998d7c4e01f815b6f49
Aman kumar <amankumar105541@gmail.com>
2025-03-29 09:19:48 +0530

    update database

--- src/appwrite/database.js
+++ src/appwrite/database.js
@@ -4,12 +4,68 @@ import { Client,ID,Databases,Storage,Query} from 'appwrite';
 
 export class Service {
     client=new Client();
-    Databases;
+    databases;
     bucket;
     constructor(){
         this.client
         setEndPoint(config.appwriteurl)
         setProject(config.appwriteProjectId);
+        this.databases=new Databases(this.client);
+        this.bucket=new Storage(this.client);
+    }
+
+    async createPost({title,slug,content,featuredImage,status,userId}){
+        try {
+return await this.databases.createDocument(
+    config.appwriteDatabaseId,
+    config.appwriteCollectionId,
+    slug,
+    {
+        title,
+        content,
+        featuredImage,
+        status,
+        userId,
+    }
+
+)
+        } catch (error) {
+            console.log("appwrite error",error)
+        }
+    }
+
+    async updatePost(slug,{title,content,featuredImage,status}){
+        try {
+return await this.databases.updateDocument(
+    config.appwriteDatabaseId,
+    config.appwriteCollectionId,
+    slug,
+    {
+        title,
+        content,
+        featuredImage,
+        status,
+    }
+
+)
+        } catch (error) {
+            console.log("appwrite error",error)
+        }
+    }
+
+
+    async deletePost(slug){
+        try {
+     await this.databases.deleteDocument(
+    config.appwriteDatabaseId,
+    config.appwriteCollectionId,
+    slug
+)
+    return true
+        } catch (error) {
+            console.log("appwrite error",error);
+            return false
+        }
     }
 }
 

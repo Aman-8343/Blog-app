febb102f7ea0d67009c54178ef693627e2e21db5
Aman kumar <amankumar105541@gmail.com>
2025-03-30 16:00:14 +0530

    update

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -5,4 +5,19 @@ const initialState={
     userData:null
 }
 
-const authSlice =createSlice({});
+const authSlice =createSlice({
+    name:"auth",
+    initialState,
+    reducers:{
+        login:(state,action)=>{
+            state.status=true;
+            state.userData=action.payload.userData;
+        },
+        logout:(state)=>{
+            state.status=false;
+            state.userData=null;
+        }
+    }
+});
+
+export default authSlice.reducer;

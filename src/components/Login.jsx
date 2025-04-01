11d9586f661d3ebdeee4ff211f767b72cd856dc5
Aman kumar <amankumar105541@gmail.com>
2025-04-01 23:14:51 +0530

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -61,7 +61,18 @@ function Login() {
                     }
                 })}
                 />
-               
+                <Input
+                label="Password: "
+                type="password"
+                placeholder="Enter your password"
+                {...register("password", {
+                    required: true,
+                })}
+                />
+                <Button
+                type="submit"
+                className="w-full"
+                >Sign in</Button>
             </div>
         </form>
         </div>

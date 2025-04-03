9aaeab0746ec693629336effde1c925d479fb54f
Aman kumar <amankumar105541@gmail.com>
2025-04-03 10:03:33 +0530

    creating router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
+import { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { createBrowserRouter } from 'react-router-dom'
+import { AuthLayout, Login } from './components/index.js'
 
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element: <App/>,
+    Children:[
+      {
+        path:'/',
+       element:<Home/>,
+      },
+      {
+        path:"Login",
+        element:(
+          <AuthLayout authentication={false}>
+            <Login/>
+            </AuthLayout>
+        )
+      }
+
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
